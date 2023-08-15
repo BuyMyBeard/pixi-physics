c4e246949b647060bbd96b2edb6c0e67f60cdb84
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-08-15 19:04:18 -0400

    raycast circle intersection

--- src/Physics/Raycast.ts
+++ src/Physics/Raycast.ts
@@ -1,5 +1,6 @@
-import { Point } from 'pixi.js';
+import { Circle, Point } from 'pixi.js';
 import { Body } from '../Body/Body';
+import { Segment } from '../Utils/MathUtils';
 
 export class Raycast
 {
@@ -11,5 +12,34 @@ export class Raycast
         {
             minDistance = 0;
         }
+    }
+    public static lineIntersectCircle([s1, s2] : Segment, circle : Circle) : Point[]
+    {
+        const delta = s2.subtract(s1);
+        const lengthSqr = delta.magnitudeSquared();
+        const determinant = ((s1.x - circle.x) * (s2.y - circle.y)) - ((s2.x - circle.x) * (s1.y - circle.y));
+
+        const incidence = (circle.radius * circle.radius * lengthSqr) - (determinant * determinant);
+
+        if (incidence < 0) return [];
+        if (incidence === 0)
+        {
+            const x = determinant * delta.y / (lengthSqr);
+            const y = -determinant * delta.x / (lengthSqr);
+
+            return [new Point(x + circle.x, y + circle.y)];
+        }
+        const rightX = Math.sign(delta.y) * delta.x * Math.sqrt(incidence) / lengthSqr;
+        const rightY = Math.abs(delta.y) * Math.sqrt(incidence) / lengthSqr;
+
+        const leftX = determinant * delta.y / lengthSqr;
+        const leftY = -determinant * delta.x / lengthSqr;
+
+        const x1 = leftX + rightX;
+        const x2 = leftX - rightX;
+        const y1 = leftY + rightY;
+        const y2 = leftY - rightY;
+
+        return [new Point(x1 + circle.x, y1 + circle.y), new Point(x2 + circle.x, y2 + circle.y)];
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { Debug } from './Utils/Debug';
 import { CircleBody } from './Body/CircleBody';
 import { Layers } from './Physics/Layers';
 import { BinaryTree } from './Utils/BinaryTree';
+import { Raycast } from './Physics/Raycast';
 
 export const app = new Application({
     view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
@@ -22,7 +23,7 @@ export const app = new Application({
 // const bt = new BinaryTree<string>();
 // const data = Array.from('In computer science, an abstract syntax tree (AST), or just syntax tree, is a tree representation of the abstract syntactic structure of text (often source code) written in a formal language. Each node of the tree denotes a construct occurring in the text. The syntax is "abstract" in the sense that it does not represent every detail appearing in the real syntax, but rather just the structural or content-related details. For instance, grouping parentheses are implicit in the tree structure, so these do not have to be represented as separate nodes. Likewise, a syntactic construct like an if-condition-then statement may be denoted by means of a single node with three branches. This distinguishes abstract syntax trees from concrete syntax trees, traditionally designated parse trees. Parse trees are typically built by a parser during the source code translation and compiling process. Once built, additional information is added to the AST by means of subsequent processing, e.g., contextual analysis. Abstract syntax trees are also used in program analysis and program transformation systems.');
 
-
+console.log(Raycast.lineIntersectCircle([new Point(0, 0), new Point(1, 1)], new Circle(0, 0.5, 1)));
 
 Layers.addLayer(1, 'balls', true);
 Layers.addLayer(2, 'polygons', true);
